c1ce1120c064f8e4bd1902f38cdb34e5d22a98dc
ds526-student <damionsklenarsclare@gmail.com>
2026-04-15 16:16:17 +1200

    commenting
    added comments to the LineReader and CopyTest classes

--- CopyTest.java
+++ CopyTest.java
@@ -1,10 +1,13 @@
 import java.io.*;
 
+/**
+ * test program that uses the LineReader class and prints them to an output file
+ */
 public class CopyTest {
     public static void main(String[] args) throws Exception {
         LineReader reader = new LineReader(System.in);
         String line;
-        
+
         while ((line = reader.ReadLine()) != null) {
             System.out.print(line);
         }
--- LineReader.java
+++ LineReader.java
@@ -1,16 +1,27 @@
 import java.io.*;
 
+/**
+ * reads one character at a time until it reaches the end of a line
+ */
 public class LineReader {
     private InputStream input;
 
+    /**
+     * @param input the input stream to read from
+     */
     public LineReader(InputStream input) {
         this.input = input;
     }
 
+    /**
+     * reads a single line from the input
+     * @return the line read, or null if the end of the stream is reached
+     */
     public String ReadLine() throws IOException {
         StringBuilder line = new StringBuilder();
         int c;
 
+        // loops until it reaches the end of a line or the stream
         while ((c = input.read()) != -1) {
             line.append((char) c);
 
@@ -19,6 +30,7 @@ public class LineReader {
             }
         }
 
+        // if the line is not empty, return it
         if (line.length() > 0) return line.toString();
         
         return null;
